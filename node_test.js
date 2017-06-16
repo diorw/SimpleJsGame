8ab21b84b4c865e6d09bd7a576c730a26ff68514
Wu Di Ao <406714601@qq.com>
2017-06-16 15:24:45 +0800

    排行榜实现
    用sqlite

--- node_test.js
+++ node_test.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');//引入文件读取模块
 
-var documentRoot = 'E:/dior/js/SimpleJsGame-master';
+var documentRoot = 'D:\\diorw\\css\\simpleJsGame';
 //需要访问的文件的存放目录
 
 var server= http.createServer(function(req,res){
