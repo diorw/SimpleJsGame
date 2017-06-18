4099d49d2e870b374f6f94a18f0146ed5ad94e14
Wu Di Ao <406714601@qq.com>
2017-06-18 13:38:20 +0800

    express框架 前后端数据传输

--- server.js
+++ server.js
@@ -1,28 +1,69 @@
-var express = require('express');
+var express = require('express'),http=require('http');
+var bodyParser=require('body-parser');
+var sqlite3 = require('sqlite3');
 var app = express();
-
+var db = new sqlite3.Database('databases/wda.sqlite3');
 app.use(express.static('D:/diorw/css/simpleJsGame'));
-
 app.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 })
+app.use(bodyParser.urlencoded({extend:false}));
+app.use(bodyParser.json());
 
-app.get('/process_get', function (req, res) {
+app.get('/json', function (req, res,next) {
 
-   // 输出 JSON 格式
-   var response = {
-       "first_name":req.query.first_name,
-       "last_name":req.query.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
+
+
+  /* res.end(JSON.stringify(response));*/
+  /* var statement=db.prepare("insert into ranktable(name,rows,times) values(?,?,?)");
+   statement.run(req.query.Name,rows,times);
+   statement.finalize();*/
+
+   db.all("select * from ranktable",function(err,Res){
+     if(!err){
+
+      //  console.log(Res);//Res是一个对象数组。
+         res.send(Res);
+      //  response=JSON.stringify(Res[0]);
+
+    //    console.log(response);
+     }else{
+        console.log(err);
+     }
+   });
+
+
 })
+app.post('/service',function(req,res){
 
+
+  var rows = parseInt(req.query.rows);
+  var times = parseInt(req.query.times);
+  var statement=db.prepare("insert into ranktable(name,rows,times) values(?,?,?)");
+  statement.run(req.query.names,rows,times);
+  statement.finalize();
+  res.send(req.query.names);
+
+});
+/*app.get('/json',function(req,res,next){
+  res.send({status:'jsoup'});
+});*/
 var server = app.listen(8081, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var io = require('socket.io');
+  var fs = require("fs");
+  if (!fs.existsSync("databases"))
+  {
+    fs.mkdirSync("databases", function (err)
+    {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+  }
 
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
 
+//  db.run("create table ranktable(name varchar(15) not null,rows int not null,times int not null)",function(err,res){
+//  });
 })
